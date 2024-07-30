8fae3e3a8115990a158c6316c970b706989f3e10
Arda Toraman <toramanarda245@gmail.com>
2024-07-31 01:33:25 +0300

    first commit

---
+++ assets/js/posts.js
@@ -0,0 +1,17 @@
+async function getData(postId) {
+  let post = await fetch('https://dummyjson.com/post/' + postId).then(res => res.json());
+  let { comments } = await fetch(`https://dummyjson.com/post/${postId}/comments`).then(res => res.json());
+
+  const postContentItem = document.querySelector('.postContentItem');
+  postContentItem.innerHTML += `<div class="postContent">
+        <h2>${post.title}</h2> 
+        <p>${post.body}</p>
+        <ul id="list" class="list"></ul>
+       </div>`;
+
+  list.innerHTML += comments.map(x => `<li>${x.user.fullName} said : ${x.body}</li>`).join('');
+}
+
+for (let i = 1; i <= 30; i++) {
+  getData(i);
+}
